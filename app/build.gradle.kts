555154c60096a3579499b9ca9aafd2b0a781ac34
Cavs <miguelferreiranoss@gmail.com>
2026-09-02 23:02:35 +0100

    Adicionar app/build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,13 @@
+plugins { id("com.android.application") }
+
+android {
+    namespace = "com.mypoupar.simuladord2d"
+    compileSdk = 35
+    defaultConfig {
+        applicationId = "com.mypoupar.simuladord2d"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+}
